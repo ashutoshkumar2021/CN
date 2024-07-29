f880fdfe77fb99d4f54f17f82986eaf29d656133
Ashutosh Kumar <80633589+ashutoshkumar2021@users.noreply.github.com>
2024-07-30 00:41:24 +0530

    Create Day1.cpp

---
+++ Arrays/Day1.cpp
@@ -0,0 +1,49 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    //array declare
+    // int blue=7;
+    // cout<<&blue<<endl;
+    // int arr[]={2,4,6,9,12};
+    // int parth[7]={1,2};
+    // int crr[3]={1,2,3,4,5};
+    // cout<<arr<<endl;
+    // cout<<&arr<<endl;
+    // cout<<"Array created successfully"<<endl;
+    
+    // int a[]={1,3,5,7,9};
+    // // cout<<a[2]<<endl;
+    
+    // for(int index=0;index<5;index++){
+    //     cout<<a[index]<<" ";
+    // }
+    // int n;
+    // cin>>n;
+    // //Bad practice
+    // int arr[5]={1,3,5,7,9};
+    
+    //static array
+    // int arr[10000];
+    // cout<<"Input the values in array"<<endl;
+    
+    // for(int i=0;i<;i++){
+    //     cin>>arr[i];
+    // }
+    
+    
+    //printing
+   
+    // for(int i=0;i<5;i++){
+    //     cout<<arr[i]*2<<" ";
+    // }
+    
+    int arr[10]={0};
+    
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
