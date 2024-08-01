897a5d8b43446f80b01cf791f303a8269da8df12
Ashutosh Kumar <80633589+ashutoshkumar2021@users.noreply.github.com>
2024-08-01 23:02:44 +0530

    Update Day3.cpp

--- Arrays/Day3.cpp
+++ Arrays/Day3.cpp
@@ -28,3 +28,32 @@ int main() {
     }
     return 0;
 }
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr{1,2,3,3,4,6,8};
+    vector<int> brr{3,3,4,10};
+    
+    vector<int> ans;
+    
+    //outer loop
+    for(int i=0;i<arr.size();i++){
+        int element=arr[i];
+        //for every element , run loop on brr
+        for(int j=0;j<brr.size();j++){
+            if(element==brr[j]){
+                //flag or mark
+                brr[j]=-1;
+                ans.push_back(element);
+            }
+        }
+    }
+    
+    //print ans
+    for(auto value : ans){
+        cout<<value<<" ";
+    }
+    return 0;
+}
