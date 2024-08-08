aad899563e7fc17dab8a10526333f4f57c313b6f
Ashutosh Kumar <80633589+ashutoshkumar2021@users.noreply.github.com>
2024-08-08 07:37:11 +0530

    Create peakmountain.cpp

---
+++ Binary Search/peakmountain.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int findPeakIndex(vector<int> arr){
+        int s=0;
+        int e=arr.size()-1;
+        int mid=s+(e-s)/2;
+
+        while(s<e){
+            if(arr[mid]<arr[mid+1]){
+                //right search
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+            mid=s+(e-s)/2;
+        }
+        return s;
+    }
+    int peakIndexInMountainArray(vector<int>& arr) {
+        return findPeakIndex(arr);
+    }
+};
