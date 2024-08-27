0d25a171e7a525244c2f801a0bf28f96702d87ed
Ashutosh Kumar <80633589+ashutoshkumar2021@users.noreply.github.com>
2024-08-27 22:57:32 +0530

    Create charArrays.cpp

---
+++ Pointers/charArrays.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int arr[5]={1,2,3,4,5};
+    // char ch[6]="abcde";
+    // cout<< arr <<endl;
+    // cout<< ch <<endl;
+    
+    // char *c=&ch[0];
+    // //print entire strings
+    // cout<<c<<endl;
+
+    char temp='z';
+    char *p2=&temp;
+    
+    cout<<p2<<endl;
+    
+    return 0;
+}
